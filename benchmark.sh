f1ae123cdbf27334769de86636d7f95b9f29f83e
Victor Poughon <victor.poughon@cnes.fr>
2016-09-14 15:44:46 +0200

    add bandmath, fix int16 issue

--- benchmark.sh
+++ benchmark.sh
@@ -19,6 +19,20 @@ time gdal_calc.py \
 --calc="(A+B+C+D+E+F+G+H+I)/9.0" \
 --outfile=landsat_average_gdal_calc.tif
 
+# OTB BandMath
+time otbcli_BandMath -il \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140309.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140401.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140417.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140528.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140620.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140731.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20140901.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20141003.tif \
+Data/LANDSAT_MultiTempIm_clip_GapF_20141026.tif \
+-out landsat_average_otb_BandMath.tif int16 \
+-exp "(im1b1 + im2b1 + im3b1 + im4b1 + im5b1 + im6b1 + im7b1 + im8b1 + im9b1)/9.0"
+
 # OTB BandMathX
 time otbcli_BandMathX -il \
 Data/LANDSAT_MultiTempIm_clip_GapF_20140309.tif \
@@ -30,5 +44,5 @@ Data/LANDSAT_MultiTempIm_clip_GapF_20140731.tif \
 Data/LANDSAT_MultiTempIm_clip_GapF_20140901.tif \
 Data/LANDSAT_MultiTempIm_clip_GapF_20141003.tif \
 Data/LANDSAT_MultiTempIm_clip_GapF_20141026.tif \
--out landsat_average_otb.tif \
+-out landsat_average_otb_BandMathX.tif int16 \
 -exp "(im1b1 + im2b1 + im3b1 + im4b1 + im5b1 + im6b1 + im7b1 + im8b1 + im9b1)/9.0"
